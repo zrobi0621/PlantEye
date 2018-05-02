b2a4bec929eeafa4ce4db6b0c16a864437b66bf8
zrobi0621 <zrobi0621@gmail.com>
2018-05-02 23:28:42 +0200

    Trackbar improvement

--- PlantEye/Form1.Designer.cs
+++ PlantEye/Form1.Designer.cs
@@ -39,9 +39,32 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.redTrackBar = new System.Windows.Forms.TrackBar();
+            this.redTextBox = new System.Windows.Forms.TextBox();
+            this.rLabel = new System.Windows.Forms.Label();
+            this.greenTrackBar = new System.Windows.Forms.TrackBar();
+            this.blueTrackBar = new System.Windows.Forms.TrackBar();
+            this.gLabel = new System.Windows.Forms.Label();
+            this.bLabel = new System.Windows.Forms.Label();
+            this.greenTextBox = new System.Windows.Forms.TextBox();
+            this.blueTextBox = new System.Windows.Forms.TextBox();
+            this.originalLabel = new System.Windows.Forms.Label();
+            this.rgbPictureBox = new System.Windows.Forms.PictureBox();
+            this.percentTrackBar = new System.Windows.Forms.TrackBar();
+            this.percentTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.Debug = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rgbPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.percentTrackBar)).BeginInit();
             this.SuspendLayout();
             // 
             // statusStrip1
@@ -88,9 +111,9 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 85);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 115);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(565, 390);
+            this.pictureBox1.Size = new System.Drawing.Size(134, 107);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
@@ -113,7 +136,6 @@
             this.checkBox1.TabIndex = 3;
             this.checkBox1.Text = "Highlighting";
             this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // label1
             // 
@@ -132,7 +154,6 @@
             this.label2.Size = new System.Drawing.Size(57, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Bad spots:";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // pictureBox2
             // 
@@ -140,16 +161,191 @@
             this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox2.Location = new System.Drawing.Point(601, 85);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(565, 390);
+            this.pictureBox2.Size = new System.Drawing.Size(543, 362);
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // redTrackBar
+            // 
+            this.redTrackBar.Location = new System.Drawing.Point(160, 85);
+            this.redTrackBar.Maximum = 255;
+            this.redTrackBar.Name = "redTrackBar";
+            this.redTrackBar.Size = new System.Drawing.Size(265, 45);
+            this.redTrackBar.TabIndex = 6;
+            this.redTrackBar.Scroll += new System.EventHandler(this.redTrackBar_Scroll);
+            // 
+            // redTextBox
+            // 
+            this.redTextBox.Location = new System.Drawing.Point(467, 85);
+            this.redTextBox.Name = "redTextBox";
+            this.redTextBox.Size = new System.Drawing.Size(48, 20);
+            this.redTextBox.TabIndex = 7;
+            this.redTextBox.TextChanged += new System.EventHandler(this.redTextBox_TextChanged);
+            // 
+            // rLabel
+            // 
+            this.rLabel.AutoSize = true;
+            this.rLabel.Location = new System.Drawing.Point(432, 88);
+            this.rLabel.Name = "rLabel";
+            this.rLabel.Size = new System.Drawing.Size(18, 13);
+            this.rLabel.TabIndex = 8;
+            this.rLabel.Text = "R:";
+            // 
+            // greenTrackBar
+            // 
+            this.greenTrackBar.Location = new System.Drawing.Point(160, 115);
+            this.greenTrackBar.Maximum = 255;
+            this.greenTrackBar.Name = "greenTrackBar";
+            this.greenTrackBar.Size = new System.Drawing.Size(265, 45);
+            this.greenTrackBar.TabIndex = 6;
+            this.greenTrackBar.Scroll += new System.EventHandler(this.greenTrackBar_Scroll);
+            // 
+            // blueTrackBar
+            // 
+            this.blueTrackBar.Location = new System.Drawing.Point(160, 147);
+            this.blueTrackBar.Maximum = 255;
+            this.blueTrackBar.Name = "blueTrackBar";
+            this.blueTrackBar.Size = new System.Drawing.Size(265, 45);
+            this.blueTrackBar.TabIndex = 6;
+            this.blueTrackBar.Scroll += new System.EventHandler(this.blueTrackBar_Scroll);
+            // 
+            // gLabel
+            // 
+            this.gLabel.AutoSize = true;
+            this.gLabel.Location = new System.Drawing.Point(432, 117);
+            this.gLabel.Name = "gLabel";
+            this.gLabel.Size = new System.Drawing.Size(18, 13);
+            this.gLabel.TabIndex = 8;
+            this.gLabel.Text = "G:";
+            // 
+            // bLabel
+            // 
+            this.bLabel.AutoSize = true;
+            this.bLabel.Location = new System.Drawing.Point(431, 147);
+            this.bLabel.Name = "bLabel";
+            this.bLabel.Size = new System.Drawing.Size(17, 13);
+            this.bLabel.TabIndex = 8;
+            this.bLabel.Text = "B:";
+            // 
+            // greenTextBox
+            // 
+            this.greenTextBox.Location = new System.Drawing.Point(467, 114);
+            this.greenTextBox.Name = "greenTextBox";
+            this.greenTextBox.Size = new System.Drawing.Size(48, 20);
+            this.greenTextBox.TabIndex = 7;
+            this.greenTextBox.TextChanged += new System.EventHandler(this.greenTextBox_TextChanged);
+            // 
+            // blueTextBox
+            // 
+            this.blueTextBox.Location = new System.Drawing.Point(467, 144);
+            this.blueTextBox.Name = "blueTextBox";
+            this.blueTextBox.Size = new System.Drawing.Size(48, 20);
+            this.blueTextBox.TabIndex = 7;
+            this.blueTextBox.TextChanged += new System.EventHandler(this.blueTextBox_TextChanged);
+            // 
+            // originalLabel
+            // 
+            this.originalLabel.AutoSize = true;
+            this.originalLabel.Location = new System.Drawing.Point(10, 88);
+            this.originalLabel.Name = "originalLabel";
+            this.originalLabel.Size = new System.Drawing.Size(45, 13);
+            this.originalLabel.TabIndex = 8;
+            this.originalLabel.Text = "Original:";
+            // 
+            // rgbPictureBox
+            // 
+            this.rgbPictureBox.Location = new System.Drawing.Point(537, 82);
+            this.rgbPictureBox.Name = "rgbPictureBox";
+            this.rgbPictureBox.Size = new System.Drawing.Size(50, 50);
+            this.rgbPictureBox.TabIndex = 9;
+            this.rgbPictureBox.TabStop = false;
+            // 
+            // percentTrackBar
+            // 
+            this.percentTrackBar.Location = new System.Drawing.Point(202, 198);
+            this.percentTrackBar.Maximum = 100;
+            this.percentTrackBar.Name = "percentTrackBar";
+            this.percentTrackBar.Size = new System.Drawing.Size(265, 45);
+            this.percentTrackBar.TabIndex = 6;
+            this.percentTrackBar.Value = 10;
+            this.percentTrackBar.Scroll += new System.EventHandler(this.percentTrackBar_Scroll);
+            // 
+            // percentTextBox
+            // 
+            this.percentTextBox.Location = new System.Drawing.Point(473, 202);
+            this.percentTextBox.Name = "percentTextBox";
+            this.percentTextBox.Size = new System.Drawing.Size(48, 20);
+            this.percentTextBox.TabIndex = 7;
+            this.percentTextBox.TextChanged += new System.EventHandler(this.percentTextBox_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(534, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "%";
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(601, 468);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(75, 23);
+            this.refreshButton.TabIndex = 11;
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // Debug
+            // 
+            this.Debug.Location = new System.Drawing.Point(61, 371);
+            this.Debug.Multiline = true;
+            this.Debug.Name = "Debug";
+            this.Debug.Size = new System.Drawing.Size(344, 91);
+            this.Debug.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 371);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Debug:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(158, 202);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Offset:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1193, 572);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Debug);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rgbPictureBox);
+            this.Controls.Add(this.bLabel);
+            this.Controls.Add(this.gLabel);
+            this.Controls.Add(this.originalLabel);
+            this.Controls.Add(this.rLabel);
+            this.Controls.Add(this.percentTextBox);
+            this.Controls.Add(this.blueTextBox);
+            this.Controls.Add(this.greenTextBox);
+            this.Controls.Add(this.redTextBox);
+            this.Controls.Add(this.percentTrackBar);
+            this.Controls.Add(this.blueTrackBar);
+            this.Controls.Add(this.greenTrackBar);
+            this.Controls.Add(this.redTrackBar);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBox1);
@@ -166,6 +362,11 @@
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rgbPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.percentTrackBar)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -184,6 +385,24 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TrackBar redTrackBar;
+        private System.Windows.Forms.TextBox redTextBox;
+        private System.Windows.Forms.Label rLabel;
+        private System.Windows.Forms.TrackBar greenTrackBar;
+        private System.Windows.Forms.TrackBar blueTrackBar;
+        private System.Windows.Forms.Label gLabel;
+        private System.Windows.Forms.Label bLabel;
+        private System.Windows.Forms.TextBox greenTextBox;
+        private System.Windows.Forms.TextBox blueTextBox;
+        private System.Windows.Forms.Label originalLabel;
+        private System.Windows.Forms.PictureBox rgbPictureBox;
+        private System.Windows.Forms.TrackBar percentTrackBar;
+        private System.Windows.Forms.TextBox percentTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.TextBox Debug;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- PlantEye/Form1.cs
+++ PlantEye/Form1.cs
@@ -13,15 +13,37 @@ namespace PlantEye
 {
     public partial class Form1 : Form
     {
-
+        Bitmap origipic;
         Bitmap pic;
         float sumPixels;
         float bad = 0;
+        int red = 0;
+        int green = 0;
+        int blue = 0;
+        int redTopLimit = 0;
+        int redBottomLimit = 0;
+        int greenTopLimit = 0;
+        int greenBottomLimit = 0;
+        int blueTopLimit = 0;
+        int blueBottomLimit = 0;
+        int percent = 10;
+        Bitmap miniRGB = new Bitmap(50, 50);
+        Color highlightColor = Color.Red;
 
         public Form1()
         {
             InitializeComponent();
             this.Text = "PlantEye";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            colorMiniRGBbox(red, green, blue);
+
+            redTextBox.Text = red.ToString();
+            greenTextBox.Text = green.ToString();
+            blueTextBox.Text = blue.ToString();
+            percentTextBox.Text = percent.ToString();
 
         }
 
@@ -32,9 +54,7 @@ namespace PlantEye
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
-
-          
+        { 
             //openFileDialog1.InitialDirectory = "c:\\";
             //openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
             //openFileDialog1.FilterIndex = 2;
@@ -45,12 +65,10 @@ namespace PlantEye
             ofdlg.Title = "Open Image";
             //ofdlg.Filter = "bmp files (*.bmp)|*.bmp";
             
-
-
             if (ofdlg.ShowDialog() == DialogResult.OK)
             {
                 //PictureBox PictureBox1 = new PictureBox();
-                Bitmap origipic = new Bitmap(ofdlg.FileName);
+                origipic = new Bitmap(ofdlg.FileName);
                 pic = new Bitmap(ofdlg.FileName);
                 pictureBox1.Image = origipic;
                 pictureBox2.Image = pic;
@@ -64,44 +82,122 @@ namespace PlantEye
 
             }
 
+            findBadPixels(0,10, 0,10, 0,10);
+            
+        }
 
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            findLimits();
+            findBadPixels(redBottomLimit, redTopLimit, greenBottomLimit, greenTopLimit, blueBottomLimit, blueTopLimit);
+        }
+
+        //  FIND BAD PIXELS
+        void findBadPixels(int redBottomLimit ,int redTopLimit, int greenBottomLimit,int greenTopLimit, int blueBottomLimit, int blueTopLimit)
+        {
+            bad = 0;
+            pic = (Bitmap)origipic.Clone();
             for (int x = 0; x < pic.Width; x++)
             {
                 for (int y = 0; y < pic.Height; y++)
                 {
                     Color pixelColor = pic.GetPixel(x, y);
-                    if (pixelColor.R >100 && pixelColor.R < 200 && pixelColor.G > 70 && pixelColor.G < 170 && pixelColor.B < 160 && pixelColor.B > 70)
+                    if (pixelColor.R > redBottomLimit && pixelColor.R < redTopLimit && pixelColor.G > greenBottomLimit && pixelColor.G < greenTopLimit && pixelColor.B  > blueBottomLimit && pixelColor.B < blueTopLimit)
                     {
-                        Color myColor = Color.Red;
-                        pic.SetPixel(x, y, myColor);
+                        pic.SetPixel(x, y, highlightColor);
                         bad++;
                     }
                 }
             }
-
-            label2.Text = "Bad spots: " + bad/sumPixels*100 + " %";
-
-
+            pictureBox2.Image = pic;
+            label2.Text = "Bad spots: " + bad / sumPixels * 100 + " %";
         }
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+
+        //  COLORING MINI RGB BOX
+        void colorMiniRGBbox(int red, int green, int blue)
+        {
+            
+            for (int x = 0; x < 50; x++)
+            {
+                for (int y = 0; y < 50; y++)
+                {
+                    miniRGB.SetPixel(x, y, Color.FromArgb(red, green, blue));
+                }
+            }
+            rgbPictureBox.Image = miniRGB;
+        }
+
+        //  FIND LIMITS
+        void findLimits()
+        {
+            redTopLimit = red + (red * percent / 100);
+            redBottomLimit = red - (red * percent / 100);
+            greenTopLimit = green + (green * percent / 100);
+            greenBottomLimit = green - (green * percent / 100);
+            blueTopLimit = blue + (blue * percent / 100);
+            blueBottomLimit = blue - (blue * percent / 100);
+            Debug.Text = redBottomLimit + " " + redTopLimit;
+        }
+
+        //  TRACKBARS
+        private void redTrackBar_Scroll(object sender, EventArgs e)
         {
 
+            red = redTrackBar.Value;
+            redTextBox.Text = red.ToString();
+            colorMiniRGBbox(red, green, blue);
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void greenTrackBar_Scroll(object sender, EventArgs e)
         {
-
+            green = greenTrackBar.Value;
+            greenTextBox.Text = green.ToString();
+            colorMiniRGBbox(red, green, blue);
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void blueTrackBar_Scroll(object sender, EventArgs e)
         {
-
+            blue = blueTrackBar.Value;
+            blueTextBox.Text = blue.ToString();
+            colorMiniRGBbox(red, green, blue);
         }
 
-        private void pictureBox2_Click(object sender, EventArgs e)
+        private void percentTrackBar_Scroll(object sender, EventArgs e)
         {
+            percent = percentTrackBar.Value;
+            percentTextBox.Text = percent.ToString();
 
         }
+
+        //TEXTBOX_TEXTCHANGED
+        private void redTextBox_TextChanged(object sender, EventArgs e)
+        {
+            red = int.Parse(redTextBox.Text);
+            redTrackBar.Value = red;
+            colorMiniRGBbox(red, green, blue);
+        }
+
+        private void greenTextBox_TextChanged(object sender, EventArgs e)
+        {
+            green = int.Parse(greenTextBox.Text);
+            greenTrackBar.Value = green;
+            colorMiniRGBbox(red, green, blue);
+        }
+
+        private void blueTextBox_TextChanged(object sender, EventArgs e)
+        {
+            blue = int.Parse(blueTextBox.Text);
+            blueTrackBar.Value = blue;
+            colorMiniRGBbox(red, green, blue);
+        }
+
+        private void percentTextBox_TextChanged(object sender, EventArgs e)
+        {
+            percent = int.Parse(percentTextBox.Text);
+            percentTrackBar.Value = percent;
+
+        }
+
     }
 }
